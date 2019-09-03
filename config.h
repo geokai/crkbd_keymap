7ceb4c757f77ec7bc25f442fac1365f6c45afb3f
George Kaimakis <geoptus@gmail.com>
2019-09-03 14:51:51 +0100

    Additional rules and configurations:
          modified:   config.h
          modified:   rules.mk

--- config.h
+++ config.h
@@ -18,29 +18,50 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
+/*
+This file was adapted on 09/03/19
+Amended by: George Kaimakis - https://github.com/geokai
+*/
+
+
 #pragma once
 
-//#define USE_MATRIX_I2C
 
-/* Select hand configuration */
-
+/* Select handing configuration */
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define SSD1306OLED
-
+/* Select connection mode */
+//#define USE_MATRIX_I2C
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+/* Select display */
+#define SSD1306OLED
 
+// #define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 200
+
+/* Confugure RGB light effects */
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
-    #define RGBLED_NUM 27
+//    #define RGBLIGHT_ANIMATIONS
+    #define RGBLIGHT_EFFECT_BREATHING
+    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//    #define RGBLIGHT_EFFECT_KNIGHT
+    #define RGBLIGHT_EFFECT_SNAKE
+//    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+
+    #define RGBLED_NUM 21
     #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_HUE_STEP 8
+    #define RGBLIGHT_SAT_STEP 16
+    #define RGBLIGHT_VAL_STEP 16
 #endif
+
+
+/* Try to reduce firmware size */
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
--- rules.mk
+++ rules.mk
@@ -1,3 +1,34 @@
+# Build rules file
+# This file was created on 09/03/19
+# Author: George Kaimakis - https://github.com/geokai
+
+
+# Bootloader (this setting is usually not required)
+#	This definition is optional, and if your keyboard supports multiple
+#	bootloaders of different sizes, comment this out, and the correct
+#	address will be loaded automatically (+60). See bootloader.mk for
+#	all options.
+# BOOTLOADER = caterina
+
+# Build options
+#	change to "no" to disable the options, or define inthe Makefile in
+#	the appropriate keymap folder that will get included automatically
+#
+BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes	# Audio control and Systems control(+450)
+CONSOLE_ENABLE = no	# Console for debug(+400)
+NKRO_ENABLE = no	# Nkey Rollover - if this doesn't work, see here - https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no	# Enable keyboard backlight functionality
+MIDI_ENABLE = no	# MIDI controls
+AUDIO_ENABLE = no	# Audio output on port C6
+UNICODE_ENABLE = yes	# Unicode
+BLUETOOTH_ENABLE = no	# Enable Bluetooth with Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = yes	# Enable WS2812 RGB underlight
+SWAP_HANDS_ENABLE = no	# Enable one-hand typing
+
+# Do not enable SLEEP_LED_ENABLE. It uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/glcdfont.c \
